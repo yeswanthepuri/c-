40de4d67649e44e25141e84890b58d554365ccff
Yeswanth^k <epuri.yeshu@gmail.com>
2020-02-03 18:18:36 +0530

    C# learning LinQ

---
+++ sample_learning_LINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace sample_learning_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowlargestFileWithLinQ();
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void ShowlargestFileWithLinQ()
+        {
+            var path = @"C:\Angular\Angular Forms";
+            var listOfFiles = from file in new DirectoryInfo(path).GetFiles()
+                              orderby file.Length descending
+                              select file;
+            foreach (var item in listOfFiles.Take(5))
+            {
+                Console.WriteLine($"Name: {item.Name}; Size: {item.Length}");
+            }
+        }
+    }
+}
